1f1f10748e47db4d9a731efdda5588ec1f0195e5
rodrigomicheld <rodrigo.micheld@gmail.com>
2021-03-22 09:29:36 -0300

    Invertendo uma string

---
+++ Piralandia/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Piralandia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            long entrada = Int64.Parse(n);
+            char [] arr = new char[n.Length];
+            int j = 0;
+            System.Console.WriteLine(n.Length);
+
+            for (int i = n.Length; i > 0 ; i--){
+                
+                arr[j] = n[i-1];
+                j++;
+            }
+            string v = new string(arr);
+            Console.WriteLine(v);
+
+
+            
+           // string v = new string(arr);
+           // Console.WriteLine(v);
+        }
+    }
+}
