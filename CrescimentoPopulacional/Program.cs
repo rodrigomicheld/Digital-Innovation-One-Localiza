689c5329e37151bb1b644ac1df7844b961a50c1b
rodrigomicheld <rodrigo.micheld@gmail.com>
2021-03-22 09:24:04 -0300

    Programa pra calcular crescimento populacional entre duas cidades

---
+++ CrescimentoPopulacional/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace CrescimentoPopulacional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+           // double[] arrayList = new double[4];
+            int pa, pb;
+            double cpa, cpb;
+            int anos;
+
+            for (int i = 0; i < t; i++)
+            {
+                anos = 0;
+                string[] valores = Console.ReadLine().Split(' ');
+                pa = int.Parse(valores[0]);
+                pb = int.Parse(valores[1]);
+
+               //declare as variaveis corretamente
+                cpa = double.Parse(valores[2]); 
+                cpb = double.Parse(valores[3]);
+
+                while (pa <= pb)
+                {
+                    pa+=(int)((pa*cpa)/100); 
+                    pb+=(int)((pb*cpb)/100);
+
+                    anos++;          
+                   
+                    if (anos > 100)
+                    {
+                       Console.WriteLine("Mais de 1 seculo.");
+                       break;
+                    }
+                }
+                
+                if (anos <= 100)
+                {
+                   Console.WriteLine("{0} anos.",anos);
+                }
+
+            }
+
+        }
+    }
+}
